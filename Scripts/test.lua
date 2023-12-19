cc02ee84a26536ef72ffcb56eeb69c7525d25da7
gu-on <kevin@gu-on.com>
2023-12-19 08:19:39 +0000

    Trying using ext plugin

--- Scripts/test.lua
+++ Scripts/test.lua
@@ -1,6 +1,8 @@
 -- @description Test1
 -- @author guonaudio
--- @version 1.4
+-- @version 1.5
+-- @changelog
+--   Use plugin
 -- @about
 --   Test script to access function from an external file
 
@@ -11,3 +13,5 @@ dofile(scriptPath:match("@?(.*[\\|/])") .. "../Include/lib2.lua")
 reaper.ShowConsoleMsg("test1\n")
 PrintTest1()
 PrintTest2()
+
+reaper.ShowConsoleMsg("API exsists: " .. reaper.APIExists("GU_Filesystem_PathExists"))
--- Settings/settings.lua
+++ Settings/settings.lua
@@ -1,9 +1,19 @@
 -- @description Settings
 -- @author guonaudio
--- @version 1.0
+-- @version 1.1
 -- @provides
---    [nomain]../Include/lib.lua
---    [nomain]../Include/lib2.lua
+--   [arm64] reaper_gutilitiesapi-aarch64.so https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-aarch64.so
+--   [arm32] reaper_gutilitiesapi-armv7l.so https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-armv7l.so
+--   [darwin-arm64] reaper_gutilitiesapi-arm64.dylib https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-arm64.dylib
+--   [darwin64] reaper_gutilitiesapi-x86_64.dylib https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-x86_64.dylib
+--   [linux64] reaper_gutilitiesapi-x86_64.so https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-x86_64.so
+--   [linux32] reaper_gutilitiesapi-i686.so https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-i686.so
+--   [win64] reaper_gutilitiesapi-x64.dll https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-x64.dll
+--   [win32] reaper_gutilitiesapi-x86.dll https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-x86.dll
+--   [nomain]../Include/lib.lua
+--   [nomain]../Include/lib2.lua
+-- @changelog
+--   Update to include external plugins
 -- @about
 --   This script sets some settings
 
