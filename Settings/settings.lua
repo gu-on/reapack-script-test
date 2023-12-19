1113b9a25c5807a20b9b0d2cee85923698a83d11
gu-on <kevin@gu-on.com>
2023-12-19 08:32:56 +0000

    Attempting to add extension through separate file

--- Settings/settings.lua
+++ Settings/settings.lua
@@ -1,19 +1,11 @@
 -- @description Settings
 -- @author guonaudio
--- @version 1.1
+-- @version 1.2
 -- @provides
---   [linux-aarch64] reaper_gutilitiesapi-aarch64.so https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-aarch64.so
---   [linux-armv7l] reaper_gutilitiesapi-armv7l.so https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-armv7l.so
---   [darwin-arm64] reaper_gutilitiesapi-arm64.dylib https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-arm64.dylib
---   [darwin64] reaper_gutilitiesapi-x86_64.dylib https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-x86_64.dylib
---   [linux64] reaper_gutilitiesapi-x86_64.so https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-x86_64.so
---   [linux32] reaper_gutilitiesapi-i686.so https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-i686.so
---   [win64] reaper_gutilitiesapi-x64.dll https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-x64.dll
---   [win32] reaper_gutilitiesapi-x86.dll https://github.com/gu-on/GUtilitiesAPI/releases/download/v0.1/reaper_gutilitiesapi-x86.dll
 --   [nomain]../Include/lib.lua
 --   [nomain]../Include/lib2.lua
 -- @changelog
---   Update to include external plugins
+--   Removed providing external plugins
 -- @about
 --   This script sets some settings
 
