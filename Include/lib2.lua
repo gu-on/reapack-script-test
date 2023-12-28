342e268c900e0217fe77e9a5714277b7f5975f55
gu-on <kevin@gu-on.com>
2023-12-28 20:45:14 +0000

    Checking if upadting settings updates lib2

--- Include/lib2.lua
+++ Include/lib2.lua
@@ -3,3 +3,7 @@
 function PrintTest2()
     reaper.ShowConsoleMsg("lib2\n")
 end
+
+function Addition(a, b)
+    return a + b
+end
--- Settings/settings.lua
+++ Settings/settings.lua
@@ -1,12 +1,15 @@
 -- @description Settings
 -- @author guonaudio
--- @version 1.3
+-- @version 1.3.1
 -- @provides
 --   [nomain]../Include/lib.lua
 --   [nomain]../Include/lib2.lua
 -- @changelog
---   Updated library
+--   Updated lib2
 -- @about
 --   This script sets some settings
 
-reaper.ShowConsoleMsg("Applying some settings\n")
+local a = 1
+local b = 2
+
+reaper.ShowConsoleMsg(string.format("Adding %i and %i = %i\n", a, b, Addition(a, b)))
